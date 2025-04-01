5f2d6238f5f89b38d74bdaad02475369acdad027
VotreNom <VotreEmail@example.com>
2025-04-01 18:54:32 +0200

    modif App page erreur

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Accueil from './pages/Accueil';
-import Apropos from './pages/Apropos'; 
-import Entête from './composants/Entete';
-import Pieddepage from './composants/PiedDePage';  
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Accueil from "./pages/Accueil";
+import Apropos from "./pages/Apropos";
+import Erreur from "./pages/Erreur";
+import Entête from "./composants/Entete";
+import Pieddepage from "./composants/PiedDePage";
 
 function App() {
   return (
     <div>
       <Entête />
-      <Routes>  {/* Ici, tu définis les routes mais pas de Router */}
+      <Routes>
+        {" "}
+        {}
         <Route path="/" element={<Accueil />} />
         <Route path="/a-propos" element={<Apropos />} />
+        <Route path="*" element={<Erreur />} />
       </Routes>
       <Pieddepage />
     </div>
